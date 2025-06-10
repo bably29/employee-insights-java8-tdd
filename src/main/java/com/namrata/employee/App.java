764cc5e85ca7c82e24a7278eef0771134f42b62c
Namrata Chikhle <namrata.chikhle@gmail.com>
2025-06-10 21:30:47 +0100

    Initial Java 8 + TDD Maven project setup

---
+++ src/main/java/com/namrata/employee/App.java
@@ -0,0 +1,13 @@
+package com.namrata.employee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
